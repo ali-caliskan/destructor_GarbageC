e9ad7761f2d0f544e1a15131c0d3b18e1245d65f
ali-caliskan <ali.caliskan1@outlook.com>
2024-05-04 18:33:19 +0300

    destructor_GC

---
+++ destructorGarbageColl/destructorGarbageColl/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace destructorGarbageColl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 10; i++)
+                new Ornek(i);
+            // referansız nesne oluşturma.
+
+
+            Console.WriteLine("*******************");
+
+            GC.Collect();
+
+
+            Console.Read();
+        }
+    }
+
+   
+    class Ornek
+    {
+        int numara;
+        //Sınıf ismi aynı olan method constructor  metodudur. Dönüş değeri yok.
+        // ilk tetiklenecek constructor'dır.
+        // Yapıcı Method.
+        public Ornek(int id)
+        {
+            numara = id;
+            Console.WriteLine(numara + "Nesne Üretildi.");
+        }
+
+
+        #region Destructor
+        
+        //Yıkıcı metod.
+        ~Ornek()
+        {
+            Console.WriteLine(numara +"Nesne Siliniyor.");
+        }
+
+        #endregion
+    }
+
+}
